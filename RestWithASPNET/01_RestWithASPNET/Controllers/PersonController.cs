ebb17b78658ef7b0200fd02f022d9d04ea5ba6ef
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-18 14:34:32 -0300

    Ajustando o Controller

--- RestWithASPNET/01_RestWithASPNET/Controllers/PersonController.cs
+++ RestWithASPNET/01_RestWithASPNET/Controllers/PersonController.cs
@@ -1,3 +1,4 @@
+using _01_RestWithASPNET.Models;
 using _01_RestWithASPNET.Services.Implementations;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,5 +15,40 @@ namespace _01_RestWithASPNET.Controllers
             _personService = personService;
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetPerson(long id)
+        {
+            var personFound = _personService.FindById(id);
+            if (personFound is null) return NotFound();
+            return Ok(personFound);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPersons()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person is null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person is null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+         [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            var personFound = _personService.FindById(id);
+            if (personFound is null) return BadRequest();
+            return Ok(_personService.Update(personFound));
+        }
     }
 }
