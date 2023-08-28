c2546768b1666177a80c0b2964f5a430162ecec9
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-28 18:51:11 -0300

    Implementando PersonService

--- 01_RestWithAspNet/Services/PersonServiceImplementation.cs
+++ 01_RestWithAspNet/Services/PersonServiceImplementation.cs
@@ -1,13 +1,17 @@
 using _01_RestWithAspNet.model;
+using Microsoft.AspNetCore.Http.HttpResults;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _01_RestWithAspNet.Services
 {
     public class PersonServiceImplementation : IPersonSerice
     {
+        public volatile int count;
+
         Person IPersonSerice.Create(Person person)
         {
             return person;
@@ -24,13 +28,40 @@ namespace _01_RestWithAspNet.Services
             
             foreach (var item in persons)
             {
-                
+                Person person = MockPerson(item);
+                persons.Add(person);
             };
+
+            return persons;
+        }
+
+        private Person MockPerson(object i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Cyro",
+                LastName = "Cunha",
+                Address = "Rua dos bobos, 012",
+                Gender = "M"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
         }
 
         Person IPersonSerice.FindById(long id)
         {
-            throw new NotImplementedException();
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Cyro",
+                LastName = "Cunha",
+                Address = "Rua dos bobos, 012",
+                Gender = "M"
+            };
         }
 
         Person IPersonSerice.Update(Person person)
