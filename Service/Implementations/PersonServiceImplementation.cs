44af8205da4c9b799830d91f9b3cff19f9899eb7
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-05 14:08:45 -0300

    IPersonService

--- Service/Implementations/PersonServiceImplementation.cs
+++ Service/Implementations/PersonServiceImplementation.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestWithAspNet.model;
 
 namespace RestWithAspNet.Service.Implementations
 {
-    public class PersonServiceImplementation
+    public class PersonServiceImplementation : IPersonService
     {
-        
+        Person IPersonService.Create(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPersonService.Delete(long id)
+        {
+            
+        }
+
+        List<Person> IPersonService.FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Person IPersonService.FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Person IPersonService.Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
