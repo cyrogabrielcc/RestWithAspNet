6eae4d1fe2734d7feb205193a172550a34b95d21
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-05 14:07:27 -0300

    IPersonService

--- Service/IPersonService.cs
+++ Service/IPersonService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestWithAspNet.model;
 
 namespace RestWithAspNet.Service
 {
     public interface IPersonService
     {
-        
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update (Person person);
+        void Delete(long id);
     }
 }
