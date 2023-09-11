8966688286fbc58294e39a5ff871710ab1e97378
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-11 16:29:01 -0300

    ConfigureServices

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestWithAspNet.Service;
+using RestWithAspNet.Service.Implementations;
 
 namespace RestWithAspNet
 {
@@ -32,6 +27,7 @@ namespace RestWithAspNet
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithAspNet", Version = "v1" });
             });
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
