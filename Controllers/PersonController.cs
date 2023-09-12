93b95f20a24cdd7620fdc4c882e3dc57a905f07c
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-12 19:38:30 -0300

    get put delete

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestWithAspNet.model;
+using RestWithAspNet.Service;
 
 namespace RestWithAspNet.Controllers
 {
@@ -8,6 +11,50 @@ namespace RestWithAspNet.Controllers
     public class PersonController  : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService) 
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+
+
 
     }
 }
