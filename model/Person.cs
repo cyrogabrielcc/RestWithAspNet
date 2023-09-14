b98ddbbd9097ba80df122f21c90c7b352b6436bf
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-14 20:45:40 -0300

    connectdb

--- model/Person.cs
+++ model/Person.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace RestWithAspNet.model
 {
+    [Table("Person")]
     public class Person
     {
+        [Column("id")]
         public long Id { get; set; }
+        [Column("first_name")]
         public string FirstName { get; set; }
+        [Column("last-name")]
         public string LastName { get; set; }
+        [Column("address")]
         public string Address { get; set; }
+        [Column("gender")]
         public string Gender { get; set; }
     }
 }
